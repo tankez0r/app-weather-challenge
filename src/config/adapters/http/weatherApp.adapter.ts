161f22c470d5117007e630df567481b9586e6687
tankez0r <76497823+tankez0r@users.noreply.github.com>
2024-06-03 15:00:03 -0300

    Update weatherApp.adapter.ts

--- src/config/adapters/http/weatherApp.adapter.ts
+++ src/config/adapters/http/weatherApp.adapter.ts
@@ -1,6 +1,6 @@
 import { AxiosAdapter } from "./axios.adapter"
 
-const weatherBaseParams = { "appid": "64884f9cefcbee5067933ab904e44caf" }
+const weatherBaseParams = { "appid": "" }
 const weatherAppInstance = new AxiosAdapter({
     baseUrl: "http://api.openweathermap.org",
     params: weatherBaseParams
